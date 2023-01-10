eb92b3e3586af18b677af216361bfc8a8a71f24f
Oleks <archer.opendub@gmail.com>
2023-01-10 19:03:02 +0100

    TDD exercises for the odin project

---
+++ jest.test.js
@@ -0,0 +1,30 @@
+const capitalize = require("./src/capitalize");
+const calculator = require("./src/calculator");
+
+test("returns a string with a capital letter", () => {
+  expect(capitalize("project")).toBe("Project");
+});
+
+test("if a number gets passed in returns null", () => {
+  expect(capitalize(10)).toBe(null);
+});
+
+test("adds two positive numbers", () => {
+  expect(calculator.add(2, 4)).toBe(6);
+});
+
+test("adds two negative numbers", () => {
+  expect(calculator.add(-10, -1)).toBe(-11);
+});
+
+test("subtracts one number from the other", () => {
+  expect(calculator.subtract(5, 3)).toBe(2);
+});
+
+test("divides one number by other", () => {
+  expect(calculator.divide(9, 3)).toBe(3);
+});
+
+test("multiplies two numbers", () => {
+  expect(calculator.multiply(5, 2)).toBe(10);
+});
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    capitalize: "./src/capitalize.js",
+    calculator: "./src/calculator.js",
+    cipher: "./src/caesarCipher.js",
+    reverseStr: "./src/reverseString.js",
+    arrAnalyze: "./src/analyzeArray.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
