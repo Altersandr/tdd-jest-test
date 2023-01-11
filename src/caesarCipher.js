dd7a4deaac124aa16dfc7c55f6e77ced279a89a6
Oleks <archer.opendub@gmail.com>
2023-01-11 13:25:42 +0100

    still trying to figure out a way to implement case sensitivity

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -34,28 +34,37 @@ const caesarCipher = (string, number) => {
 
 const shift = (array, number) => {
   if (number > 26) number = number % 26;
+  let i = 0;
   let result = [];
   let toUpper = [];
   let reg = /[^a-zA-Z\s]|\s/;
   array.forEach((letter) => {
     if (reg.test(letter)) result.push(letter);
     else {
-      if (/[A-Z]/.test(letter)) toUpper.push(array.indexOf(letter));
-      //   console.log(array);
+      if (/[A-Z]/.test(letter)) toUpper.push(i);
       //   console.log(array.indexOf(letter));
-      let index = alphabet.indexOf(letter);
+      let index = alphabet.indexOf(letter.toLowerCase());
       if (index + number < 0) result.push(alphabet[(26 % number) + index]);
       result.push(alphabet[index + number]);
     }
+    i++;
   });
-  let final;
-  //   console.log(toUpper);
+  let final = [];
+  let preFinal = [];
+  console.log(toUpper);
   //   result = result.join("");
   toUpper.forEach((element) => {
-    console.log(element);
+    // console.log(element);
 
-    final = result.splice(element, 1, result[element].toUpperCase());
-    // final = result.slice(0, element) + result.slice(element);
+    final =
+      result.join("").slice(0, result.join("").charAt(element)) +
+      result.join("").charAt(element).toUpperCase() +
+      result.join("").slice(element + 1);
+    // preFinal = final + result.slice(element + 1);
+    // result.slice(0, element).join("") +
+    // result.join("").charAt(element).toUpperCase() +
+    // result.slice(element + 1).join("");
+    // final = final.toUpperCase();
     // console.log(result.slice(element));
     // console.log(result.slice(0, element));
   });
@@ -69,6 +78,6 @@ const shift = (array, number) => {
 //   )
 // );
 
-console.log(caesarCipher("Hello", 5));
+console.log(caesarCipher("HEllO", 5));
 
 // module.exports = caesarCipher;
